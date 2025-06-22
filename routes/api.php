fa649962ea218ad3da9e3cf37eaca135c5a59042
ALBUQUERQUE Adrien <adrialb95@gmail.com>
2025-06-23 00:57:53 +0200

    [KHP-30] chore: change useless boostrap

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::middleware('web')->group(function () {
     Route::post('/login', [AuthController::class, 'authenticate'])->name('login');
@@ -9,7 +9,6 @@ Route::middleware('web')->group(function () {
     Route::post('/register', [AuthController::class, 'register'])->name('register');
 
     Route::middleware('auth')->group(function () {
-        // include the authed routes file from auhthed_route.php
         include base_path('routes/authed_route.php');
     });
 });
