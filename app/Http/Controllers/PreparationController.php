1bef9dcbffadb05fc19a6fc95fee206f12679a28
ThomAzgo <weifang.buisson@gmail.com>
2025-07-05 02:39:25 +0200

    [KHP-35] feat: add PreparationController and route (wip)

---
+++ app/Http/Controllers/PreparationController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class PreparationController extends Controller
+{
+    public function store()
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
+}
