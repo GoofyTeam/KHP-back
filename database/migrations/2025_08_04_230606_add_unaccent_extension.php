20b823da7f5122dea8c05d42b9eba4b2bb8ca291
ALBUQUERQUE Adrien <adrialb95@gmail.com>
2025-08-05 01:17:54 +0200

    [KHP-120] chore: better search in graphql

---
+++ database/migrations/2025_08_04_230606_add_unaccent_extension.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::statement('CREATE EXTENSION IF NOT EXISTS unaccent');
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        DB::statement('DROP EXTENSION IF EXISTS unaccent');
+    }
+};
