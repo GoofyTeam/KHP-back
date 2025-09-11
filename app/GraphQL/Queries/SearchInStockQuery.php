9cefa095d6a5e046c5f3b43ee639efaf0eb5ccaa
Dest.Com <37459465+DestroyCom@users.noreply.github.com>
2025-09-12 00:10:46 +0200

    [KHP-000] feat: add combined search query (#118)
    * [KHP-000] fix(back): rename search query
    
    * Apply automatic changes
    
    * [KHP-000] fix(back): rm comment
    
    ---------
    
    Co-authored-by: ThomAzgo <weifang.buisson@gmail.com>

---
+++ app/GraphQL/Queries/SearchInStockQuery.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\GraphQL\Queries;
+
+use App\Models\Ingredient;
+use App\Models\Preparation;
+
+class SearchInStockQuery
+{
+    public function resolve(mixed $_, array $args): array
+    {
+        $keyword = $args['keyword'] ?? '';
+
+        $ingredients = Ingredient::forCompany()
+            ->search($keyword)
+            ->get()
+            ->map(fn ($ingredient) => [
+                'type' => 'ingredient',
+                'ingredient' => $ingredient,
+            ]);
+
+        $preparations = Preparation::forCompany()
+            ->search($keyword)
+            ->get()
+            ->map(fn ($preparation) => [
+                'type' => 'preparation',
+                'preparation' => $preparation,
+            ]);
+
+        return $ingredients->concat($preparations)->all();
+    }
+}
