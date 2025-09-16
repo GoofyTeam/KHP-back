77edc6c6528b870909e824af8060e7c952157f57
Adrien Albuquerque <adrialb95@gmail.com>
2025-09-16 19:39:05 +0200

    [KHP-237] feat: Add computed price accessors for orders and steps

--- app/Models/StepMenu.php
+++ app/Models/StepMenu.php
@@ -7,6 +7,10 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * @property Menu|null $menu
+ * @property int $quantity
+ */
 class StepMenu extends Model
 {
     /** @use HasFactory<\Database\Factories\StepMenuFactory> */
